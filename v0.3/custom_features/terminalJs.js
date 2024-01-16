59875f357e9983dd7e9dbaa39b3c63d47514da31
Shrawan Choudhary <shrawan07090@gmail.com>
2024-01-17 01:01:29 +0530

    update

--- v0.3/custom_features/terminalJs.js
+++ v0.3/custom_features/terminalJs.js
@@ -1,18 +1,27 @@
-consoleText('> ', 'show portfolio-menu', 'text-edit');
-async function consoleText(prefix, word, id) {
-    await new Promise(w => setTimeout(w, 2000))
+consoleText(['> ', '', '> '], ['echo $welcome_message', 'Welcome to my portfolio website', 'show portfolio-menu', ], ['text1', 'text2', 'text3']);
+async function consoleText(prefixes, words, id) {
+    await new Promise(w => setTimeout(w, 100))
     document.getElementById("loader").remove()
     document.getElementById("landingPage").classList.remove("hidden")
     var visible = true;
     var con = document.getElementById('console');
     letterCount = 1
-    target = document.getElementById(id)
+    index = 0
+    target = document.getElementById(id[index])
+    done = false
     window.setInterval(async function() {
-        if (letterCount > word.length) {
+        if (done == true) {
+            // do nothing
+        } else if (index >= words.length) {
+            done = true
             await new Promise(w => setTimeout(w, 500))
             document.getElementById("menu").classList.remove("hidden")
+        } else if (letterCount > words[index].length) {
+             index += 1
+             letterCount = 1
+             target = document.getElementById(id[index])
         } else {
-            target.innerHTML = prefix + word.substring(0, letterCount) + (letterCount == word.length ? "" : "_")
+            target.innerHTML = prefixes[index] + words[index].substring(0, letterCount) + ((index != 1 && letterCount < words[index].length) ? "_" : "")
             letterCount += 1
         }
     }, 120);
